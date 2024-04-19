ac2538abc9a432a84c30c1966ab114ece3989bb2
ifty_cloud <ifty_cloud@126.com>
2024-04-19 09:23:32 +0800

    fix: 页面迁移

--- app/vitejs-vite-qinglong/src/views/home/index.vue
+++ app/vitejs-vite-qinglong/src/views/home/index.vue
@@ -1,14 +1,10 @@
 <script setup>
-import Rain from "@/components/Rain/index.vue";
-import Snow from "@/components/Snow/index.vue";
-import LeetCode from "@/components/LeetCode/index.vue";
+
 </script>
 
 <template>
   <div class="w-full h-full">
-    <!--<Rain />-->
-    <!--<Snow />-->
-    <LeetCode />
+
   </div>
 </template>
 
