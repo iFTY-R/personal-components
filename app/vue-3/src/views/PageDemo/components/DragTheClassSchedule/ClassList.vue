2f3abd9c8cabfd69e273d1d400ad73aabb6b4473
ifty_cloud <ifty_cloud@126.com>
2025-01-04 11:53:18 +0800

    feat: 拖拽课程表 demo

---
+++ app/vue-3/src/views/PageDemo/components/DragTheClassSchedule/ClassList.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+const lessonList = [
+  { label: '语文', color: '#EF5350' },
+  { label: '数学', color: '#EC407A' },
+  { label: '物理', color: '#AB47BC' },
+  { label: '化学', color: '#7E57C2' },
+  { label: '生物', color: '#5C6BC0' },
+  { label: '历史', color: '#42A5F5' },
+  { label: '地理', color: '#29B6F6' },
+  { label: '美术', color: '#26C6DA' },
+  { label: '音乐', color: '#26A69A' },
+  { label: '体育', color: '#66BB6A' },
+]
+
+</script>
+
+<template>
+<div class="flex flex-col items-center gap-10px py-10px">
+  <div
+      v-for="item in lessonList"
+      :key="item.label"
+      :style="{ backgroundColor: item.color }"
+      class="px-20px py-10px font-bold cursor-pointer"
+  >
+    {{ item.label }}
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/vue-3/src/views/PageDemo/components/DragTheClassSchedule/拖拽课程表.vue
@@ -0,0 +1,27 @@
+<script setup>
+import ClassTable from '@/views/PageDemo/components/DragTheClassSchedule/ClassTable.vue';
+import ClassList from '@/views/PageDemo/components/DragTheClassSchedule/ClassList.vue';
+
+defineOptions({
+  name: 'DragTheClassSchedule'
+})
+
+</script>
+
+<template>
+  <div class="w-1920px">
+    <div class="py-30px text-30px font-bold text-black text-center">课程表</div>
+    <div class="flex gap-15px">
+      <div class="bg-gray-100 w-100px ">
+        <ClassList />
+      </div>
+      <div class="bg-gray-100 w-900px">
+        <ClassTable />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
